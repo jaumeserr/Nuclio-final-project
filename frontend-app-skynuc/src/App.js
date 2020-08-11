af621dd4807800d14e50f1484d44ef0cca7813b0
Ruth C Morata Gil <ruth.morata@gmail.com>
2020-08-11 18:01:20 +0200

    React Router Dom implemented, but buttonflightsearch still not working with it

--- frontend-app-skynuc/src/App.js
+++ frontend-app-skynuc/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import SimpleReactLightbox from 'simple-react-lightbox';
-import Home from './pages/home/home.view';
 import './App.css';
 import './assets/fonts.css';
 import FlightListPage from './pages/flightlistpage/flightlistpage.view';
+import Home from './pages/home/home.view';
+import { FLIGHT_LIST, HOME } from './routes/routes';
 
 function App() {
     return (
-        <div>
-            {/* <SimpleReactLightbox>
-            <Home/>
-            </SimpleReactLightbox> */}
-            {/* <FlightCard /> */}
-            <FlightListPage />
-        </div>
+        <Router>
+
+            <SimpleReactLightbox>
+                <Home/>
+            </SimpleReactLightbox>
+
+                <Switch>
+                    <Route exact path={HOME}>
+                        <Home />
+                    </Route>
+                    <Route path={FLIGHT_LIST}>
+                        <FlightListPage />
+                    </Route>
+                </Switch>
+                
+        </Router>
     );
 }
 
