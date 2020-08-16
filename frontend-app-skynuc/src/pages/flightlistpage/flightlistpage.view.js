113c431f2d18159f6299b14cdbe7dfaf44b96a80
Ruth C Morata Gil <ruth.morata@gmail.com>
2020-08-17 00:06:49 +0200

    multiple fetch using Promise implemented BUT NOT WORKING yet

--- frontend-app-skynuc/src/pages/flightlistpage/flightlistpage.view.js
+++ frontend-app-skynuc/src/pages/flightlistpage/flightlistpage.view.js
@@ -6,16 +6,44 @@ import FlightCard from '../../components/flightcard/flightCard.view';
 import Navbar from '../../components/navbar/navbar.view';
 import styles from './flightlistpage.module.css';
 
+
+const baseUrl = 'http://localhost/api';
+
 const FlightListPage = () => {
 
     const [infoFlights, setInfoFlights] = useState([]);
 
     useEffect(() => {
-        const url = 'http://localhost/api/cities';
         const options = {
             method: 'GET',
             headers: new Headers(),
         };
+
+
+        // --> MORE THAN ONE FETCH
+        // SOURCE (Promise): https://gomakethings.com/waiting-for-multiple-all-api-responses-to-complete-with-the-vanilla-js-promise.all-method/
+
+        // Promise.all([
+        //     // fetch('http://localhost/api/airlines', options),
+        //     fetch('http://localhost/api/cities', options)
+        // ]).then(response => {
+        //     return Promise.all(response.map(response => {
+        //         return response.json();
+        //     }));
+        // }).then(payload => {
+        //     console.log(payload);
+        //     console.log("Data from DB loaded");
+        //     setInfoFlights(payload);
+        // }).catch(error => {
+        //     console.log(error);
+        // });
+
+
+
+
+        // --> ONE SINGLE FETCH
+
+        const url = `${baseUrl}/cities`;
 
         fetch(url, options)
             .then(response => {
@@ -25,11 +53,16 @@ const FlightListPage = () => {
                     return Promise.reject(response.status);
                 })
             .then(payload => {
+                    console.log(payload);
                     console.log("Data from DB loaded");
                     setInfoFlights(payload);
                 })
             .catch(error => console.log(error));
+
+
     }, []);
+
+
 
     const breakpointColumnsObj = {
         default: 1,
