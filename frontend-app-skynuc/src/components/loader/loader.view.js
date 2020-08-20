7828ba1b8aebfd8eca8a3ca792c90bf39003befb
Ruth C Morata Gil <ruth.morata@gmail.com>
2020-08-20 21:20:56 +0200

    background color issue fixed

--- frontend-app-skynuc/src/components/loader/loader.view.js
+++ frontend-app-skynuc/src/components/loader/loader.view.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Spinner } from 'react-awesome-spinners';
 import styles from './loader.module.css';
-import { Spinner } from 'react-awesome-spinners'
 
 function Loader() {
     return (
         <div className={styles.__loader__container}>
             <div className={styles.__loader}>
-                <Spinner color='#2B71E4' />
+                <Spinner color='#2B71E4' size={12} />
+                {/* FIXME: Size does not work. Issue to corresponding github repo posted  */}
             </div>
         </div>
     );
