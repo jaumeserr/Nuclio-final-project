e10b72b3b9ce67212624683d8bba9e3507409b32
Hosman <tgi_hosman@hotmail.com>
2020-08-24 18:18:37 +0200

    modificacion de scripts de create table y seed populate

---
+++ backend-api-skynuc/database/seeds/B_populate_tables_dummy/B_001_populate_table_dummy_airports.sql
@@ -0,0 +1,12 @@
+INSERT INTO airports(iata,city_name) VALUES ('AMS','Amsterdam');
+INSERT INTO airports(iata,city_name) VALUES ('BCN','Barcelona');
+INSERT INTO airports(iata,city_name) VALUES ('CDG','Paris');
+INSERT INTO airports(iata,city_name) VALUES ('DUS','Düsseldorf');
+INSERT INTO airports(iata,city_name) VALUES ('EIN','Eindhoven');
+INSERT INTO airports(iata,city_name) VALUES ('FRA','Frankfurt');
+INSERT INTO airports(iata,city_name) VALUES ('LIS','Lisbon');
+INSERT INTO airports(iata,city_name) VALUES ('MAD','Madrid');
+INSERT INTO airports(iata,city_name) VALUES ('MGL','Düsseldorf');
+INSERT INTO airports(iata,city_name) VALUES ('MRS','Marseille');
+INSERT INTO airports(iata,city_name) VALUES ('OPO','Porto');
+INSERT INTO airports(iata,city_name) VALUES ('ORY','Paris');
